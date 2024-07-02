3e288df6e331f1a10dcbb4176a154d1c7e5220ef
KIM DAGEON <rlaekdms9312789@gmail.com>
2024-07-02 19:21:04 +0900

    feat : 플레이어 조작
    좌우 이동과 점프 완성

--- Assets/02. Script/Player/PlayerMovement.cs
+++ Assets/02. Script/Player/PlayerMovement.cs
@@ -1,23 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody2D rb;
+    private Rigidbody2D rb;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 8f;
+    private bool isFacingRight = true;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal*speed,rb.velocity.y);        
+    }
     void Update()
     {
-        
-    }
-    
-    void Move()
-    {
 
+
+        if (!isFacingRight && horizontal > 0f)
+        {
+            Flip();
+        }
+        else if (isFacingRight && horizontal < 0f)
+        {
+            Flip();
+        }
+    }
+    //점프
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(context.performed && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        /*if(context.canceled && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower * 0.5f);
+        }*/
+    }
+    //바닥에 있을때만 점프가능
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+    //캐릭터 좌우 반전
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+    //이동
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
     }
 }
